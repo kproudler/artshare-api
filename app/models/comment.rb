853bef3e510b6229007bc02bfb1a5137ca9b2ed6
Kevin Proudler <kevinproudler@gmail.com>
2021-02-05 08:53:32 -0500

    fixed model + added seeds

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,9 +2,9 @@ class Comment < ApplicationRecord
     validates :body, presence: true
 
     belongs_to :author,
-        foreign_key: user_id,
+        foreign_key: :user_id,
         class_name: 'User'
     belongs_to :artwork,
-        foreign_key: artwork_id,
+        foreign_key: :artwork_id,
         class_name: 'Artwork'
 end
